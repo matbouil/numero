b3dc342a1ed88803db8164abbe56f76639a26041
Matthieu <matthieu@paofit.com>
2019-10-04 18:35:16 +0200

    Fix compilation issues

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -26,7 +26,30 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-func testConversionForTwo() {
-  let result = converter.convert(2)
-  XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+  var converter: Converter!
+  
+  override func setUp() {
+    super.setUp()
+    converter = Converter()
+  }
+  
+  override func tearDown() {
+    converter = nil
+    super.tearDown()
+  }
+  
+  func testConversionForOne() {
+    let result = converter.convert(1)
+    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+  }
+  
+  func testConversionForTwo() {
+    let result = converter.convert(2)
+    XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
+  }
+  
 }
